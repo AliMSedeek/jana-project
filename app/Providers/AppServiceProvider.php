735dbd68f8ee57461de1391f9bc351ec1a24e418
Ali Mohamed Sedeek <120599804+AliMSedeek@users.noreply.github.com>
2026-04-26 09:46:31 +0000

    fix https css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        URL::forceScheme('https');
-    }
+{
+    \Illuminate\Support\Facades\URL::forceScheme('https');
+}
 }
