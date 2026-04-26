cb04273685aefa54c5e3ecc6a01724abbc45f7a9
Ali Mohamed Sedeek <120599804+AliMSedeek@users.noreply.github.com>
2026-04-26 10:06:30 +0000

    fix https css

--- resources/views/app/app.blade.php
+++ resources/views/app/app.blade.php
@@ -2,7 +2,8 @@
 <html>
 <head>
     <title>Cake Shop</title>
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <script src="{{ asset('script.js') }}" defer></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 </head>
 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
