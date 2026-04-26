37c3cc9d50be5637617596e551bcd83c4a71adc9
Ali Mohamed Sedeek <120599804+AliMSedeek@users.noreply.github.com>
2026-04-26 10:00:08 +0000

    fix https css

--- resources/views/app/app.blade.php
+++ resources/views/app/app.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Cake Shop</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 </head>
 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
